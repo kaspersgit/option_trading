09ae888d1807b98d04043b65db8d90b52baa9d76
kasper.de-harder <kasper.de-harder@klarna.com>
2020-06-26 16:53:25 +0200

    Added code from other repo for sending email, not activated

--- cherry_pick.py
+++ cherry_pick.py
@@ -15,4 +15,40 @@ df = pd.read_csv('barchart_unusual_activity_2020-06-24.csv')
 cherry_df = cherry_pick(df, OutOfMoney = 1.1, maxDTE = 14, minVolOIrate = 3)
 selected_options_df = cherry_df[['baseSymbol','baseLastPrice','symbolType','strikePrice','expirationDate','lastPrice', 'volume', 'openInterest',
        'volumeOpenInterestRatio','current_date']]
-selected_options_df.to_csv('options_high_volumeOIratio_20200624.csv')
+#selected_options_df.to_csv('options_high_volumeOIratio_20200624.csv')
+
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Email configurations and content
+recipients = ['kasperde@hotmail.com']
+emaillist = [elem.strip().split(',') for elem in recipients]
+msg = MIMEMultipart()
+msg['Subject'] = "Scientias summaries"
+msg['From'] = 'k.sends.python@gmail.com'
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    {0}
+  </body>
+</html>
+""".format(selected_options_df.to_html())
+
+part1 = MIMEText(html, 'html')
+msg.attach(part1)
+
+# Sending the email
+import smtplib, ssl
+
+port = 465  # For SSL
+password = open("/home/pi/Documents/trusted/ps_gmail_send.txt", "r").read()
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(msg['From'], password)
+    server.sendmail(msg['From'], emaillist , msg.as_string())
