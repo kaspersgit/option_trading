e598def8921165d8acb8ce2fe6e18ba938219839
kasper.de-harder <kasper.de-harder@klarna.com>
2020-12-11 09:49:58 +0100

    Moved recipents to seperate file and added high profitable options

--- predicting/predicting.py
+++ predicting/predicting.py
@@ -12,7 +12,7 @@ import pickle
 # Load newest data
 today = datetime.today().strftime("%Y-%m-%d")
 current_path = os.getcwd()
-df = pd.read_csv(current_path+'Documents/python_scripts/option_trading/data/barchart/barchart_unusual_activity_'+today+'.csv')
+df = pd.read_csv(current_path+'/Documents/python_scripts/option_trading/data/barchart/barchart_unusual_activity_'+today+'.csv')
 
 # Select model
 model_choice = 'AdaBoost'
@@ -35,7 +35,7 @@ df['const'] = 1.0
 # Load model and predict
 if model_choice == 'LogisticRegression':
     # Logistic Regression
-    file_path = current_path + '/predicting/modelLogit'
+    file_path = current_path + '/Docuements/python_scripts/option_trading/predicting/modelLogit'
     model = LogitResults.load(file_path)
     model_name = file_path.split('/')[-1]
     # Select columns which are model needs as input but leave out the constant
@@ -51,14 +51,14 @@ elif model_choice == 'CatBoost':
     features = model.feature_names_
     prob = model.predict_proba(df[features])[:,1]
 elif model_choice == 'AdaBoost':
-	file_path = current_path + '/trained_models/c_AB_v1.sav'
+	file_path = current_path + '/Documents/python_scripts/option_trading/trained_models/c_AB_v1.sav'
 	with open(file_path, 'rb') as file:
 		model = pickle.load(file)
 	model_name = file_path.split('/')[-1]
 	features = model.feature_names
 	prob = model.predict_proba(df[features])[:, 1]
 elif model_choice == 'GradientBoost':
-	file_path = current_path + '/trained_models/c_GB_v1.sav'
+	file_path = current_path + '/Documents/python_scripts/option_trading/trained_models/c_GB_v1.sav'
 	with open(file_path, 'rb') as file:
 		model = pickle.load(file)
 	model_name = file_path.split('/')[-1]
@@ -70,21 +70,38 @@ df['prediction'] = prob
 df['model'] = model_name
 
 # %%
-# Subsetting the predictions
+# Subsetting the predictions for highly probable stocks
 threshold = 0.6
 maxBasePrice = 200
 minDaysToExp = 3
 maxDaysToExp = 20
 minStrikeIncrease = 1.05
 
-buy_advise = df[(df['prediction'] > threshold) &
+high_prob = df[(df['prediction'] > threshold) &
     (df['symbolType']=='Call') &
     (df['daysToExpiration'] < maxDaysToExp) &
     (df['priceDiffPerc'] > minStrikeIncrease) &
     (df['daysToExpiration'] > minDaysToExp) &
-    (df['baseLastPrice'] < maxBasePrice)]
-buy_advise = buy_advise[['baseSymbol', 'predDate', 'expirationDate', 'baseLastPrice', 'strikePrice', 'priceDiffPerc', 'prediction','model']]
-buy_advise = buy_advise.sort_values('priceDiffPerc').reset_index(drop=True)
+    (df['baseLastPrice'] < maxBasePrice)].copy()
+high_prob = high_prob[['baseSymbol', 'predDate', 'expirationDate', 'baseLastPrice', 'strikePrice', 'priceDiffPerc', 'prediction','model']]
+high_prob = high_prob.sort_values('priceDiffPerc').reset_index(drop=True)
+
+
+# Subsetting the predictions for highly profitable stocks
+hprof_threshold = 0.1
+hprof_maxBasePrice = 1000
+hprof_minDaysToExp = 3
+hprof_maxDaysToExp = 20
+hprof_minStrikeIncrease = 1.20
+
+high_prof = df[(df['prediction'] > hprof_threshold) &
+    (df['symbolType']=='Call') &
+    (df['daysToExpiration'] < hprof_maxDaysToExp) &
+    (df['priceDiffPerc'] > hprof_minStrikeIncrease) &
+    (df['daysToExpiration'] > hprof_minDaysToExp) &
+    (df['baseLastPrice'] < hprof_maxBasePrice)].copy()
+high_prof = high_prof[['baseSymbol', 'predDate', 'expirationDate', 'baseLastPrice', 'strikePrice', 'priceDiffPerc', 'prediction','model']]
+high_prof = high_prof.sort_values('priceDiffPerc').reset_index(drop=True)
 
 # %%
 # Sending an email with the predictions
@@ -92,7 +109,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 # Email configurations and content
-recipients = ['kasperde@hotmail.com','derekdh@gmail.com']
+with open('/home/pi/Documents/trusted/option_predict_email_receivers.txt') as f:
+	recipients = f.read().splitlines()
 emaillist = [elem.strip().split(',') for elem in recipients]
 msg = MIMEMultipart()
 msg['Subject'] = "Stock buy advise"
@@ -102,19 +120,32 @@ html = """\
 <html>
   <head></head>
   <body>
+  	<h3> High probability </h3>
     {0}
-    <hr>
-    <h3> Configurations </h3>
+    <h4> Configurations </h4>
     <p>
     Minimal threshold: {1} <br>
     Maximum stock price: {2} <br>
     Days to expiration between {3} and {4} <br>
     Strike price at least {5} higher than stock price <br>
-    </p
+    </p>
+    <hr>
+    <h3> High profitability </h3>
+    {6}
+    <h4> Configurations </h4>
+    <p>
+    Minimal threshold: {7} <br>
+    Maximum stock price: {8} <br>
+    Days to expiration between {9} and {10} <br>
+    Strike price at least {11} higher than stock price <br>
+    </p>
   </body>
 </html>
-""".format(buy_advise.to_html(),threshold,maxBasePrice,
-  minDaysToExp,maxDaysToExp,minStrikeIncrease)
+""".format(high_prob.to_html(),threshold,maxBasePrice,
+  minDaysToExp,maxDaysToExp,minStrikeIncrease,
+		   high_prof.to_html(),hprof_threshold, hprof_maxBasePrice,
+  hprof_minDaysToExp,hprof_maxDaysToExp,h_profminStrikeIncrease,
+		   )
 
 part1 = MIMEText(html, 'html')
 msg.attach(part1)
