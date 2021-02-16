bc6511fbdc5358a593041a61d3274f7b0b8096ae
Kasper de Harder <kasper.de-harder@klarna.com>
2021-02-16 20:33:08 +0100

    Adjusted scheduled bash files to check if aws sync is working

--- other_files/rpi_scheduled_scripts/barchart_scrape.sh
+++ other_files/rpi_scheduled_scripts/barchart_scrape.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+echo "Starting barchart scraping..."
 python3 /home/pi/Documents/python_scripts/option_trading/scraping/barchart_scraping.py >> /home/pi/scripts/barchart_scraping.log
-aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/barchart/. s3://project-option-trading/raw_data/barchart/
+echo "Barchart scraping... Done"
+echo "Starting s3 sync..."
+aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/barchart/. s3://project-option-trading/raw_data/barchart/ >> /home/pi/scripts/barchart_scraping.log
+echo "s3 sync... Done"
--- other_files/rpi_scheduled_scripts/marketbeat_scrape.sh
+++ other_files/rpi_scheduled_scripts/marketbeat_scrape.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+echo "Starting marketbeat scraping..."
 python3 /home/pi/Documents/python_scripts/option_trading/scraping/marketbeat_scraping.py >> /home/pi/scripts/marketbeat_scraping.log
-aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/
+echo "Marketbeat scraping... Done"
+echo "Starting s3 sync..."
+aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/ >> /home/pi/scripts/marketbeat_scraping.log
+echo "s3 sync... Done"
