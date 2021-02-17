6d20515af5437c13cd0f8275a0fe0a37564d4620
Kasper de Harder <kasper.de-harder@klarna.com>
2021-02-17 09:01:04 +0100

    Added full path to aws command

--- other_files/rpi_scheduled_scripts/bash_test.sh
+++ other_files/rpi_scheduled_scripts/bash_test.sh
@@ -3,5 +3,5 @@ echo "Kasper"
 echo "is...... Great"
 python3 -c "print('this is within python')"
 echo "Starting s3 sync..."
-aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/ >> /home/pi/scripts/marketbeat_scraping.log
+/home/pi/.local/bin/aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/ >> /home/pi/scripts/marketbeat_scraping.log
 echo "s3 sync... Done"
--- other_files/rpi_scheduled_scripts/marketbeat_scrape.sh
+++ other_files/rpi_scheduled_scripts/marketbeat_scrape.sh
@@ -3,5 +3,5 @@ echo "Starting marketbeat scraping..."
 python3 /home/pi/Documents/python_scripts/option_trading/scraping/marketbeat_scraping.py >> /home/pi/scripts/marketbeat_scraping.log
 echo "Marketbeat scraping... Done"
 echo "Starting s3 sync..."
-aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/ >> /home/pi/scripts/marketbeat_scraping.log
+/home/pi/.local/bin/aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/marketbeat/. s3://project-option-trading/raw_data/marketbeat/ >> /home/pi/scripts/marketbeat_scraping.log
 echo "s3 sync... Done"
