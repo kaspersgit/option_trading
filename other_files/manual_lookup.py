213ae9bc94e023beafa9666d98ea1a807a49aaf4
Kasper de Harder <kasper.de-harder@klarna.com>
2021-03-09 10:40:59 +0100

    Added scoring to look up script

--- other_files/manual_lookup.py
+++ other_files/manual_lookup.py
@@ -8,8 +8,12 @@ os.chdir("/home/pi/Documents/python_scripts/option_trading")
 
 from option_trading_nonprod.aws import *
 
-ticker = sys.argv[1]
-date = sys.argv[2]
+# model (disregard extension)
+model = sys.argv[1]
+model = model.split('.')[0]
+
+ticker = sys.argv[2]
+date = sys.argv[3]
 
 # Set source and target for bucket and keys
 source_bucket = 'project-option-trading'
@@ -19,11 +23,28 @@ source_key = 'raw_data/barchart/barchart_unusual_activity_{}.csv'.format(date)
 print('Source bucket: {}'.format(source_bucket))
 print('Source key: {}'.format(source_key))
 
+# set working directory
+os.chdir('/home/pi/Documents/python_scripts/option_trading')
+current_path = os.getcwd()
+file_path = current_path + '/trained_models/'+model+'.sav'
+# load in model
+with open(file_path, 'rb') as file:
+	model = pickle.load(file)
+model_name = file_path.split('/')[-1]
+features = model.feature_names
+prob = model.predict_proba(df[features])[:, 1]
+
+print('Options contract scored')
+df['prediction'] = prob
+df['model'] = model_name
+
 # import data
 #df = load_from_s3(profile="default", bucket=source_bucket, key_prefix=source_key)
 try:
 	df = pd.read_csv('data/barchart/barchart_unusual_activity_{}.csv'.format(date))
 	print(df[df['baseSymbol'] == ticker])
+	print("Interesting columns")
+	print(df[df['baseSymbol'] == ticker][['strikePrice','prediction']])
 except:
 	print('No scraped data found')
 
