0f7508666e4c6304e1e03a0e837e6252c3515890
Kasper de Harder <kasper.de-harder@klarna.com>
2021-03-09 10:48:06 +0100

    added pickle and scoring after data is imported

--- other_files/manual_lookup.py
+++ other_files/manual_lookup.py
@@ -2,6 +2,7 @@
 from dateutil.relativedelta import relativedelta, FR
 import os
 import sys
+import pickle
 import pandas as pd
 
 os.chdir("/home/pi/Documents/python_scripts/option_trading")
@@ -24,7 +25,6 @@ print('Source bucket: {}'.format(source_bucket))
 print('Source key: {}'.format(source_key))
 
 # set working directory
-os.chdir('/home/pi/Documents/python_scripts/option_trading')
 current_path = os.getcwd()
 file_path = current_path + '/trained_models/'+model+'.sav'
 # load in model
@@ -32,17 +32,20 @@ with open(file_path, 'rb') as file:
 	model = pickle.load(file)
 model_name = file_path.split('/')[-1]
 features = model.feature_names
-prob = model.predict_proba(df[features])[:, 1]
 
-print('Options contract scored')
-df['prediction'] = prob
-df['model'] = model_name
 
 # import data
 #df = load_from_s3(profile="default", bucket=source_bucket, key_prefix=source_key)
 try:
 	df = pd.read_csv('data/barchart/barchart_unusual_activity_{}.csv'.format(date))
 	print(df[df['baseSymbol'] == ticker])
+
+	prob = model.predict_proba(df[features])[:, 1]
+
+	print('Options contract scored')
+	df['prediction'] = prob
+	df['model'] = model_name
+
 	print("Interesting columns")
 	print(df[df['baseSymbol'] == ticker][['strikePrice','prediction']])
 except:
