9d01fa1644853ad98dae7e5e14113f79faff3295
Kasper de Harder <kasper.de-harder@klarna.com>
2021-04-08 23:14:45 +0200

    ADded barchart unusual volume scraping script

---
+++ scheduled_jobs/rpi_scheduled_scripts/barchart_unusual_volume_scrape.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting barchart unusual volume scraping..."
+python3 /home/pi/Documents/python_scripts/option_trading/scraping/barchart_unusual_volume_scraping.py >> /home/pi/scripts/barchart_unusual_volume_scraping.log
+echo "Barchart unusual volume scraping... Done"
+echo "Starting s3 sync..."
+/home/pi/.local/bin/aws s3 sync /home/pi/Documents/python_scripts/option_trading/data/barchart_unusual_volume_scraping/. s3://project-option-trading/raw_data/barchart_unusual_volume_scraping/ >> /home/pi/scripts/barchart_unusual_volume_scraping.log
+echo "s3 sync... Done"
