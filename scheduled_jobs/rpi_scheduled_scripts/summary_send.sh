c9bb7808a38b647758707573e1731b6870dfc4eb
Kasper de HArder <kasperde@hotmail.com>
2021-04-25 23:29:09 +0200

    Updated model use

--- scheduled_jobs/rpi_scheduled_scripts/summary_send.sh
+++ scheduled_jobs/rpi_scheduled_scripts/summary_send.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH='/home/pi/Documents/python_scripts/option_trading/'
 python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/weekly_summary/enrich_expired_options.py
-python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/weekly_summary/send_summary.py PROD_c_GB32_v1x3 PRODUCTION
+python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/weekly_summary/send_summary.py PROD_c_GB32_v1x4 PRODUCTION
