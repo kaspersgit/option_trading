ef12fff88413c9f7b1b36ffef0526ee01c21ea28
kasper.de-harder <kasperde@hotmail.com>
2022-07-22 17:03:58 +0200

    EBM not working due to ARM architecture on rpi and trained on intel

--- scheduled_jobs/rpi_scheduled_scripts/predicting_send.sh
+++ scheduled_jobs/rpi_scheduled_scripts/predicting_send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=${PYTHONPATH}:/home/pi/Documents/python_scripts/option_trading/
 echo "Predicting with barchart data main model"
-python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/predicting/predicting_barchart.py PROD_EBM64_v1x1 PRODUCTION
+python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/predicting/predicting_barchart.py DEV_c_GB64_v1x4 PRODUCTION
 echo "Predicting with marketbeat data"
 python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/predicting/predicting_marketbeat.py PROD_MB_10p_GB32_v1x0 PRODUCTION
