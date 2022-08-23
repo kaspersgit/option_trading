273686d39969e511580549f560935e20b0d121e6
kasper.de-harder <kasperde@hotmail.com>
2022-08-23 06:16:23 +0200

    Attempt to updated download location to fix download

--- scheduled_jobs/scraping/barchart_login_scraping.py
+++ scheduled_jobs/scraping/barchart_login_scraping.py
@@ -20,7 +20,7 @@ import random
 import platform, os
 
 # Only when running in production
-if platform.system() == 'Linux':
+if platform.system() == 'Linuxx':
     # To have some human kind of behaviour with visitin the website
     rand_wait=random.uniform(0,200)
     time.sleep(rand_wait)
@@ -41,16 +41,18 @@ if platform.system() == 'Linux':
     options.add_experimental_option("prefs", prefs)
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
     browser = webdriver.Chrome(options=options)
 
 elif platform.system() == 'Windows':
     options=webdriver.ChromeOptions()
     # Set download location
-    download_loc = "C:/Users/kaspe/Downloads/selenium_download"
+    download_loc = "C:\\Users\\kaspe\\Downloads\\selenium_download"
     prefs = {"download.default_directory": download_loc}
     options.add_experimental_option("prefs", prefs)
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
     options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     browser = webdriver.Chrome(
         executable_path=r'C:/Users/kaspe/Downloads/chromedriver_win32/chromedriver.exe', options=options)
