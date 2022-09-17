9be771e1b7d30bb06607dd8fec8fdccab0e4cc40
kasper.de-harder <kasperde@hotmail.com>
2022-09-17 21:11:48 +0200

    Added bash timestamp print

--- scheduled_jobs/rpi_scheduled_scripts/predicting_send.sh
+++ scheduled_jobs/rpi_scheduled_scripts/predicting_send.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=${PYTHONPATH}:/home/pi/Documents/python_scripts/option_trading/
+date +"%Y-%m-%d %T"
 echo "Predicting with barchart data main model"
 python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/predicting/predicting_barchart.py DEV_c_GB64_v1x4 PRODUCTION
 echo "Predicting with marketbeat data"
