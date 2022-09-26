c6484733b37641970ad4195ec6247d295047c5a5
Kasper <kasperde@hotmail.com>
2022-09-26 22:12:26 +0200

    Added pkill chromium before running script

--- scheduled_jobs/rpi_scheduled_scripts/barchart_login_scrape.sh
+++ scheduled_jobs/rpi_scheduled_scripts/barchart_login_scrape.sh
@@ -2,6 +2,9 @@
 date +"%Y-%m-%d %T"
 echo "Starting barchart login scraping..."
 . /home/pi/.bashrc
+echo "Killing any potential running chromium renderers"
+pkill -f -- "--type=renderer"
+
 python3 /home/pi/Documents/python_scripts/option_trading/scheduled_jobs/scraping/barchart_login_scraping.py
 echo "Barchart login scraping... Done"
 echo "Starting s3 sync..."
